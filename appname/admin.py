cf75b0e5c0b6d0a867299a2b68463e2d9108d315
Faizyy43 <ayurlandenterprise43@gmail.com>
2026-04-13 11:55:57 +0530

    first deploy

---
+++ appname/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . models import *
+# Register your models here.
+
+
+admin.site.register(Student)
+admin.site.register(QualificationOption)
+admin.site.register(BranchOption)
+admin.site.register(PassingYearOption)
+admin.site.register(TechnologyOption)
+admin.site.register(Course)
